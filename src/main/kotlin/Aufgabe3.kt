f2237b27d50fbf267cd57dfc9f175c89e9691cb7
adiko <dikodam@googlemail.com>
2018-04-26 12:46:14 +0200

    algo for l >> N; RNG

--- src/main/kotlin/Aufgabe3.kt
+++ src/main/kotlin/Aufgabe3.kt
@@ -1,2 +1,32 @@
 package main.kotlin
 
+import java.util.*
+import kotlin.streams.asSequence
+
+
+fun main(args: Array<String>) {
+    val numbers = generateRandomNumbers(50000, 5)
+    val (index, max) = searchMaximum(numbers, numbers.max()!!)
+    println("Die Zahl $index kommt am häufigsten vor, nämlich $max mal")
+}
+
+/**
+ * algo for l >> N
+ */
+fun searchMaximum(numbers: List<Int>, n: Int): Pair<Int, Int> {
+    val counters = Array(n + 1, { 0 })
+    var maxIndex = 0
+    for (number in numbers) {
+        counters[number]++
+        if (counters[number] > counters[maxIndex]) {
+            maxIndex = number
+        }
+    }
+    return Pair(maxIndex, counters[maxIndex])
+}
+
+fun generateRandomNumbers(length: Int, exclusiveUpperBound: Int): List<Int> {
+    return Random().ints(length.toLong(), 1, exclusiveUpperBound + 1)
+        .asSequence()
+        .toList()
+}
